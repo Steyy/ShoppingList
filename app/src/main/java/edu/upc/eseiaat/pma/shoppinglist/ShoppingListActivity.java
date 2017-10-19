e4f03ac27b593fb46abcc0d39bfaacfe18b9c9ff
Steyy <javi_areva@hotmail.com>
2017-10-19 19:45:19 +0200

    Lista inicial de prueba

--- app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
@@ -2,18 +2,44 @@ package edu.upc.eseiaat.pma.shoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class ShoppingListActivity extends AppCompatActivity {
+
+    private ArrayList <String> itemList;
+    private ArrayAdapter<String> adapter;
+
+    private ListView list;
+    private Button btn_add;
+    private EditText edit_item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
+//3cosas que necesitamos
+        list = (ListView) findViewById(R.id.list);
+        btn_add = (Button) findViewById(R.id.btn_add);
+        edit_item = (EditText) findViewById(R.id.edit_item);
 
-        ListView list = (ListView) findViewById(R.id.list);
-        Button btn_add = (Button) findViewById(R.id.btn_add);
+        itemList = new ArrayList<>();
+        itemList.add("Patatas");
+        itemList.add("Papel WC");
+        itemList.add("Zanahorias");
+        itemList.add("Copas Danone");
+
+        adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                itemList
+        );
+
+        list.setAdapter(adapter);
 
     }
 }
