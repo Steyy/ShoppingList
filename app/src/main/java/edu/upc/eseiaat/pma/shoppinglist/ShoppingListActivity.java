f51c5146b73107805e497e31c7213f643f28ece1
Steyy <javi_areva@hotmail.com>
2017-10-19 19:36:37 +0200

    Layout done

--- app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
@@ -2,6 +2,8 @@ package edu.upc.eseiaat.pma.shoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class ShoppingListActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class ShoppingListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
+
+        ListView list = (ListView) findViewById(R.id.list);
+        Button btn_add = (Button) findViewById(R.id.btn_add);
+
     }
 }
